131e97c790e1d235a7e0e07e58d61233b31db1e6
Volkan-Lord-Of-Fire <volkan.t.simsek@outlook.com>
2015-12-18 11:06:16 +0100

    Youtube vorschaubild auf maxres hinzugefügt

--- module/gallery/galleryController.js
+++ module/gallery/galleryController.js
@@ -28,6 +28,7 @@ angular.module('app.gallery', ['angular-inview', 'ngAnimate', 'angular-flexslide
 //            };
             $scope.slides = [
                 {id: 1, type: 'video', url: $sce.trustAsResourceUrl('https://www.youtube.com/embed/ktQqoXa7sc4')},
+                {id: 6, type: 'video', url: $sce.trustAsResourceUrl('//i.ytimg.com/vi/ktQqoXa7sc4/maxresdefault.jpg')},
                 {id: 2, type: 'image', url: 'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg'},
                 {id: 3, type: 'image', url: 'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg'},
                 {id: 4, type: 'image', url: 'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg'},
