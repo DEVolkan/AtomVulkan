5462ade63fcd794c739744deb976760d62fc24fe
Volkan-Lord-Of-Fire <volkan.t.simsek@outlook.com>
2015-12-23 19:06:44 +0100

    Karousel wird nun umrandet von der Farbe #54a0e5 http://www.color-hex.com/color/54a0e5 Schnelleres Rotieren der Bilder. Die Bilder werden auf 500px zusammengedrückt.

--- module/home/homeController.js
+++ module/home/homeController.js
@@ -18,18 +18,10 @@
                 $scope.title = false;
             }
         }
-        
+
         $scope.showAllSocialMediaInfos = function (){
             console.log(service.CreateCarousel());
         }
-
-        $scope.slides = [
-            {id: 1, type: 'video', url: $sce.trustAsResourceUrl('https://www.youtube.com/embed/ktQqoXa7sc4')},
-            {id: 2, type: 'image', url: $sce.trustAsResourceUrl('//i.ytimg.com/vi/ktQqoXa7sc4/maxresdefault.jpg')},
-            {id: 3, type: 'image', url: 'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg'},
-            {id: 4, type: 'image', url: 'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg'},
-            {id: 5, type: 'image', url: 'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg'},
-        ];
     }
     ;
 })(angular);
